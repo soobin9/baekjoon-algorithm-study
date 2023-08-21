7cb46d497bdfaec94f81e2c868329c8923f6cd4a
soobin9 <120175855+soobin9@users.noreply.github.com>
2023-08-21 17:59:46 +0900

    Update 수 정렬하기.py - 선택정렬 추가

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -5,5 +5,20 @@ for i in range(N):
     arr.append(int(input()))
    
 arr.sort()
+
+# 선택 정렬 
+for cur in range(N):
+    min = arr[cur]
+    
+    for i in range(cur + 1, N):
+        if min > arr[i]:
+            min = arr[i] 
+    
+    # 맨 앞에 넣기 
+    arr.remove(min)
+    arr.insert(cur, min)
+    # print(arr)
+
+
 for i in range(N):
     print(arr[i])
